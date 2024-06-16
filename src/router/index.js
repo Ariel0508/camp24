1b6fe06ef5146955ebb19d332ff048be69fb4145
Ariel <sonyt605@gmail.com>
2024-06-16 23:32:23 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import { createRouter, createWebHashHistory } from 'vue-router';
 const routes = [
   {
     path: '/',
-    component: import('../views/HomeView.vue'),
+    component: () => import('../views/HomeView.vue'),
   },
   {
     path: '/portfolio',
